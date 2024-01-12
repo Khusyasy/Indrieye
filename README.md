b57a6a5e5fd6308c92f28d8a58ebac88e47427cf
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2024-01-12 07:13:50 +0700

    chore: start project

---
+++ README.md
@@ -0,0 +1,3 @@
+# indrieye
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/indrieye/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.indrieye
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

4c233c51dc2f414605d3a40dd609cfa277198d41
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2024-01-12 07:55:58 +0700

    chore: init firebase + firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(const MainApp());
+  initFirebase();
+}
+
+Future<void> initFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
 }
 
 class MainApp extends StatelessWidget {

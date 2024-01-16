d27356439a29b8d64def5d10ed599d48196a7093
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2024-01-16 15:35:27 +0700

    feat: add TTS, to baca teks

--- lib/views/reader_result_view.dart
+++ lib/views/reader_result_view.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
-class ReaderResultView extends StatelessWidget {
+class ReaderResultView extends StatefulWidget {
   const ReaderResultView(this.result, {Key? key}) : super(key: key);
 
   final RecognizedText? result;
 
+  @override
+  State<ReaderResultView> createState() => _ReaderResultViewState();
+}
+
+class _ReaderResultViewState extends State<ReaderResultView> {
+  FlutterTts flutterTts = FlutterTts();
+
   String teksHasil() {
-    if (result == null || result!.text.isEmpty) {
+    if (widget.result == null || widget.result!.text.isEmpty) {
       return 'Tidak terdeteksi';
     }
-    return result!.text;
+    return widget.result!.text;
+  }
+
+  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage('id-ID');
+    await flutterTts.speak(text);
+  }
+
+  Future<void> _stop() async {
+    await flutterTts.stop();
+  }
+
+  @override
+  void initState() {
+    _speak(teksHasil());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _stop();
+    super.dispose();
   }
 
   @override
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import flutter_tts
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
